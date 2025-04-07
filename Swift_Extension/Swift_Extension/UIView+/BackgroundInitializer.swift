be34260a96ce4a52a794e299d82fd1600ecdd51a
JinUng41 <ingjwjw@naver.com>
2025-04-07 22:24:46 +0900

    [Feat] UIView의 백그라운드 생성 편의생성자 구현

---
+++ Swift_Extension/Swift_Extension/UIView+/BackgroundInitializer.swift
@@ -0,0 +1,26 @@
+//
+//  BackgroundInitializer.swift
+//  Swift_Extension
+//
+//  Created by 김진웅 on 4/7/25.
+//
+
+import UIKit
+
+extension UIView {
+    
+    /// 배경색을 설정하는 편의 생성자입니다.
+    ///
+    /// - Parameter backgroundColor: 설정할 배경색
+    ///
+    /// 사용 예시:
+    /// ```swift
+    /// // 빨간색 배경을 가진 뷰 생성
+    /// let redBackgroundView = UIView(backgroundColor: .red)
+    /// ```
+    convenience init(backgroundColor: UIColor) {
+        self.init()
+        
+        self.backgroundColor = backgroundColor
+    }
+}
